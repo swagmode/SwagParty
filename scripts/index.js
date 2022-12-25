32da029f22bff0a0539a78b7b07ed1b0c215d031
swagmode <83536916+swagmode@users.noreply.github.com>
2022-12-25 18:21:58 +0100

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+let topButton = document.getElementById("goTop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    $('#goTop').fadeIn(250)
+  } else {
+    $('#goTop').fadeOut(250)
+  }
+}
+
+function goTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
